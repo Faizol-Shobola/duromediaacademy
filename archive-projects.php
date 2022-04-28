cdf8bb090667196867c0b836137400f305e99159
Shobola Faizol <shobolafaizol@gmail.com>
2022-04-28 16:57:29 +0100

    updated version

--- archive-projects.php
+++ archive-projects.php
@@ -1,6 +1,6 @@
 <?php get_header(); ?>
 
-    <div class="bg-gray-100 py-14">
+    <!-- <div class="bg-gray-100 py-14">
         <div class="cont grid md:grid-cols-2 gap-10 h-">
             <div class="md:col-span-1">
                 <div class="h-30">
@@ -22,13 +22,46 @@
 					</div>
             </div>
         </div>
-    </div>
+    </div> -->
+
+    <!--  -->
+    <section class="text-white bg-gray-700">
+        <div class="max-w-screen-xl px-4 py-32 mx-auto lg:items-center lg:flex">
+            <div class="max-w-3xl mx-auto text-center">
+            <h1 class="text-3xl font-extrabold sm:text-5xl">
+                Understand User Flow.
+
+                <span class="sm:block">
+                Increase Conversion.
+                </span>
+            </h1>
+
+            <p class="max-w-xl mx-auto mt-4 sm:leading-relaxed sm:text-xl">
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nesciunt illo tenetur fuga ducimus numquam ea!
+            </p>
+
+            <div class="flex flex-wrap justify-center gap-4 mt-8">
+                <a class="block w-full px-12 py-3 text-sm font-medium text-white bg-blue-600 border border-blue-600 rounded sm:w-auto active:text-opacity-75 hover:bg-transparent hover:text-white focus:outline-none focus:ring" href="/get-started">
+                Get Started
+                </a>
+
+                <a class="block w-full px-12 py-3 text-sm font-medium text-white border border-blue-600 rounded sm:w-auto hover:bg-blue-600 active:bg-blue-500 focus:outline-none focus:ring" href="/about">
+                Learn More
+                </a>
+            </div>
+            </div>
+        </div>
+    </section>
+
+    <!--  -->
    
     <!-- Project Template -->
     <div class="project py-16">
-            <h2 class="font-semibold capitalize text-5xl pt- text-center">all project</h2>
-        <div class="max-w-4xl mx-auto px-6">
-            <?php get_template_part('includes/section','projects');?>
+        <!-- <h2 class="font-semibold capitalize text-5xl pt- text-center">all project</h2> -->
+        <div class="cont">
+            <div class="grid md:grid-cols-2 gap-10">
+                <?php get_template_part('includes/section','projects');?>
+            </div>
         </div>
 		 <div class="box pagination-box flex justify-around py-10 " role="navigation" aria-label="Pagination Navigation">
                 <?php previous_posts_link();?>
